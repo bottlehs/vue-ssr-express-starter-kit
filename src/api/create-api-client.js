3e9c4f8e5d178c4ddbea3c10b4870287e1ff9082
bottlehs <qudgns9@gmail.com>
2020-12-11 18:26:11 +0900

    log

--- src/api/create-api-client.js
+++ src/api/create-api-client.js
@@ -2,6 +2,7 @@ import Firebase from 'firebase/app'
 import 'firebase/database'
 
 export function createAPI ({ config, version }) {
+  console.log(config)
   Firebase.initializeApp(config)
   return Firebase.database().ref(version)
 }
